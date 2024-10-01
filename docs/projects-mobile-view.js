1f20bd2042a55fc3459b74310d0a4cf7a31c6fea
jeriko3323 <jerikocoz@gmail.com>
2024-10-01 15:27:25 +0800

    v3.1

--- docs/projects-mobile-view.js
+++ docs/projects-mobile-view.js
@@ -12,42 +12,40 @@ if (window.innerWidth > 1024) {
     document
         .getElementById("card-container")
         .addEventListener("click", () => {
-            let left = 1024;
+            let left = 100;
             for (let i = 2; i < box.length; i++) {
-                let current = left - 512;
-
+                let current = left - 50;
                 box[0].style.zIndex = 10;
-                box[1].style.zIndex = 10;
+                box[0].style.display = "flex";
+                box[1].style.display = "flex";
                 if (left == 0) {
-                    box[i].style.right = 1024 + "px";
+                    box[i].style.right = (cardContainer.offsetWidth) - (box[i].offsetWidth) + "px";
                     box[i].style.zIndex = 9;
-                    box[i].style.marginRight = 0 + "px";
                     let boxes = box.pop();
                     box.unshift(boxes);
                     let curtains = curtain.pop();
                     curtain.unshift(curtains);
-
                 } else {
-                    if (current == 512) {
+                    if (current == 50) {
                         box[i].style.width = 500 + "px";
                         box[i].style.height = 300 + "px";
                         box[i].style.bottom = 0;
                         box[i].style.zIndex = 100;
                         curtain[i].style.opacity = 0;
-                        console.log(curtain[i])
+                        box[i].style.right = cardContainer.offsetWidth / 2 - 250 + "px";
 
                     } else {
-                        box[i].style.width = 340 + "px";
+                        box[i].style.width = 262 + "px";
                         box[i].style.height = 200 + "px";
                         box[i].style.bottom = 20 + "px";
                         curtain[i].style.opacity = 0.75;
+
                     }
                     if (current == 0) {
-                        box[i].style.marginRight = 160 + "px";
                         box[i].style.zIndex = 8;
+                        box[i].style.right = 0;
                     }
 
-                    box[i].style.right = (current + "px");
                 }
                 left = current;
             }
